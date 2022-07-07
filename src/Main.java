9a6c77a6ccbb7186928a7b36f4ce6165bffc0545
tiagovieiraj <tiagovieiraj@gmail.com>
2022-07-07 16:48:27 -0300

    Desafio banco DIO usando Java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Tiago Vieira");
+
+        Conta cc = new ContaCorrente(cliente);
+        Conta poupanca = new ContaPoupanca(cliente);
+
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
